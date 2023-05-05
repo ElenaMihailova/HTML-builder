a3dc671b6be9c345b00d86f3f1da6f4c613b9d85
Elena <pumpkinpum@gmail.com>
2023-05-05 16:52:47 +0300

    feat: add solution task_4

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,30 +1,30 @@
 const fs=require('fs');
 const readline=require('readline');
 const rl=readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 })
 let first=false;
 
 console.log('Write something please.');
 
 rl.on('line', (input) => {
-    inputFake=input;
-    if (first==false) {
-        first=true;
-    }
-    else {
-        inputFake=`\n${input}`
-    }
-    fs.appendFile("02-write-file/new.txt", inputFake, function (err) {
-        if (err) throw err;
-    });
+	inputFake=input;
+	if (first==false) {
+		first=true;
+	}
+	else {
+		inputFake=`\n${input}`
+	}
+	fs.appendFile("02-write-file/new.txt", inputFake, function (err) {
+		if (err) throw err;
+	});
 
-    if (input=='exit') {
-        console.log('Good luck!');
-        process.exit();
-    }
+	if (input=='exit') {
+		console.log('Good luck!');
+		process.exit();
+	}
 })
 process.on('beforeExit', function () {
-    console.log('Good luck!');
+	console.log('Good luck!');
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs=require('fs');
+const path=require('path');
+const filesFolder=path.join(__dirname, 'files');
+const copyFolder=path.join(__dirname, 'files-copy');
 
+function copyDirectory() {
+  fs.rm(copyFolder, {force: true, recursive: true}, (err) => {
+    if (err) throw err;
+
+    fs.mkdir(copyFolder, {recursive: true}, err => {
+      if (err) throw err;
+
+      fs.readdir(filesFolder, (err, files) => {
+        if (err) return console.log(err);
+
+        for (let file of files) {
+          fs.copyFile(path.join(__dirname, 'files', file),
+            path.join(__dirname, 'files-copy', file), err => {
+              if (err) throw err;
+              console.log('File successfully copied.');
+            });
+        }
+      });
+    });
+  });
+}
+copyDirectory();
